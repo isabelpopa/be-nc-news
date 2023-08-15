5cd39b60a628ffeed72b68fec38138e556281f5e
isabelpopa <isabel.popa@outlook.com>
2023-08-15 15:07:42 +0100

    Solve Task 5 CORE: GET /api/articles

--- models/articles.models.js
+++ models/articles.models.js
@@ -16,3 +16,22 @@ exports.selectArticle = (article_id) => {
       }
     });
 };
+exports.selectArticles = () => {
+    return db.query(`SELECT a.author, a.title, a.article_id, a.topic, a.created_at, a.votes, a.article_img_url, COUNT(c.comment_id) AS comment_count
+    FROM articles AS a
+    LEFT JOIN comments AS c
+    ON a.article_id = c.article_id
+    GROUP BY a.article_id
+    ORDER BY a.created_at DESC`)
+    .then(({ rows }) => {
+      const topic = rows[0];
+      if (!topic) {
+        return Promise.reject({
+          status: 404,
+          msg: "Not Found",
+        });
+      } else {
+        return rows;
+      }
+    });
+  };
